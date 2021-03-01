71893f217c4039c9a5965e83ddf42eaf7cbb9db7
ZoiliSanchez <38706142+ZoilaSanchez@users.noreply.github.com>
2021-02-28 19:32:24 -0600

    Lista circular

---
+++ Proyecto 1 SO/src/listacircular/ListaCircular2.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listacircular;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.concurrent.ThreadLocalRandom;
+/**
+ *
+ * @author jenif
+ */
+public class ListaCircular2 {
+ Calendar calendario = new GregorianCalendar();
+    int segundos = calendario.get(Calendar.SECOND);
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+        ListaCircular listita = new ListaCircular();
+       
+         double random = ThreadLocalRandom.current().nextInt(5, 10 + 1);
+       
+         listita.insertar(0,200);
+         listita.insertar(1,100);
+         listita.insertar(2,300);
+         listita.insertar(3,400);
+        
+         listita.mostrar();
+         
+         listita.eliminar(3);    
+         System.out.println("-------------");
+         listita.mostrar();
+    }
+    
+}
---
+++ Proyecto 1 SO/src/listacircular/Nodo.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listacircular;
+
+/**
+ *
+ * @author jenif
+ */
+public class Nodo {
+    int dato;
+    Nodo sig;
+    int posicion;
+    public Nodo(int posi,int dato){
+       this.dato=dato;
+       this.posicion=posi;
+       sig=this; // siempre apunta a el mismo 
+    }
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void setDato(int dato) {
+        this.dato = dato;
+    }
+
+    public Nodo getSig() {
+        return sig;
+    }
+
+    public void setSig(Nodo sig) {
+        this.sig = sig;
+    }
+    
+    
+    
+    
+       
+
+    
+     
+     
+     
+}
